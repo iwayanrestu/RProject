6a11a9cd64ff13ee555d95166d084fa1cd929583
I Wayan Restu <iwayanrestu33@gmail.com>
2025-02-18 03:22:39 +0700

    JS Slider

---
+++ script.js
@@ -0,0 +1,46 @@
+// Untuk slider pertama
+let currentIndex = 0;
+const slides = document.querySelectorAll(".first-slider .slide");
+const totalSlides = slides.length;
+
+function updateSlide() {
+  const slider = document.querySelector(".first-slider .slider");
+  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  updateSlide();
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+  updateSlide();
+}
+
+// Auto slide every 3 seconds
+setInterval(nextSlide, 3000);
+
+// Untuk slider kedua
+let currentIndexSecond = 0;
+const slidesSecond = document.querySelectorAll(".second-slider .slide");
+const totalSlidesSecond = slidesSecond.length;
+
+function updateSlideSecond() {
+  const sliderSecond = document.querySelector(".second-slider .slider");
+  sliderSecond.style.transform = `translateX(-${currentIndexSecond * 100}%)`;
+}
+
+function nextSlideSecond() {
+  currentIndexSecond = (currentIndexSecond + 1) % totalSlidesSecond;
+  updateSlideSecond();
+}
+
+function prevSlideSecond() {
+  currentIndexSecond =
+    (currentIndexSecond - 1 + totalSlidesSecond) % totalSlidesSecond;
+  updateSlideSecond();
+}
+
+// Auto slide every 3 seconds for the second slider
+setInterval(nextSlideSecond, 3000);
